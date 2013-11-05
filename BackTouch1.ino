ca05950a6df050940e128f8d693933db97a12684
Gilad Fride <giladfr@gmail.com>
2013-11-05 22:24:04 +0200

    Fixed indentation

--- BackTouch1.ino
+++ BackTouch1.ino
@@ -52,16 +52,16 @@ int dx_t = 5; // dx threshold
 int sl_scrl = 5; // sleep after sending a scroll event
 int sl_nscrl = 10; // sleep after not sending a scroll event
 
-typedef enum 
+typedef enum
 {
   MODE_POINTER,
   MODE_SCROLL,
   MODE_LAST,
-  } OperationModes;
+} OperationModes;
 int cur_mode = MODE_POINTER; // default mode
 
 
-// Average 
+// Average
 
 
 
@@ -70,135 +70,143 @@ void CalcMovment_Scroll(int dx, int dy, int* hz_scrl, int *vr_scrl)
 {
   if (abs(dy) > dy_t)
   {
-        if (dy>0) // positive dy
-        {
-          *vr_scrl = 1; 
-        }
-        else
-        { 
-          *vr_scrl = -1; // up scroll should be more responsive maybe
-        }
-      }
-
-      if (abs(dx) > dx_t)
-      {
-        if (dx>0) // positive dx
-        {
-          *hz_scrl = 1; // move only one step
-        }
-        else
-        {
-          *hz_scrl = -1;
-        }
-      }
-    }
-
-
-    void CalcMovment_Pointer(int dx, int dy,int* pnt_dx,int *pnt_dy)
+    if (dy>0) // positive dy
     {
-      if (abs(dy) > dy_t)
-      { 
-        if (dy > 0)
-        {
-          *pnt_dy = 5;
+      *vr_scrl = 1;
+    }
+    else
+    {
+      *vr_scrl = -1; // up scroll should be more responsive maybe
+    }
+  }
 
-        } 
-        else
-        {
+  if (abs(dx) > dx_t)
+  {
+    if (dx>0) // positive dx
+    {
+      *hz_scrl = 1; // move only one step
+    }
+    else
+    {
+      *hz_scrl = -1;
+    }
+  }
+}
 
-          *pnt_dy = -5;
 
-        }
-      }
-
-      if (abs(dx) > dy_t)
-      {
-        *pnt_dx = dx;
-      }
-      *pnt_dx = 0;
+void CalcMovment_Pointer(int dx, int dy,int* pnt_dx,int *pnt_dy)
+{
+  if (abs(dy) > dy_t)
+  {
+    if (dy > 0)
+    {
+      *pnt_dy = 5;
 
     }
-
-
-    int read_LCD_buttons()
+    else
     {
-     adc_key_in = analogRead(0);   
-     if (adc_key_in > 1000) return btnNONE; 
-     if (adc_key_in < 50)   return btnRIGHT;  
-     if (adc_key_in < 195)  return btnUP; 
-     if (adc_key_in < 380)  return btnDOWN; 
-     if (adc_key_in < 555)  return btnLEFT; 
-     if (adc_key_in < 790)  return btnSELECT;    
 
-     return btnNONE; 
-   }
+      *pnt_dy = -5;
 
-   int LCD_action()
-   {
-    lcd.setCursor(0,0);
+    }
+  }
+
+  if (abs(dx) > dy_t)
+  {
+    *pnt_dx = dx;
+  }
+  *pnt_dx = 0;
+
+}
+
+
+int read_LCD_buttons()
+{
+  adc_key_in = analogRead(0);
+  if (adc_key_in > 1000) return btnNONE;
+  if (adc_key_in < 50)   return btnRIGHT;
+  if (adc_key_in < 195)  return btnUP;
+  if (adc_key_in < 380)  return btnDOWN;
+  if (adc_key_in < 555)  return btnLEFT;
+  if (adc_key_in < 790)  return btnSELECT;
+
+  return btnNONE;
+}
+
+int LCD_action()
+{
+  lcd.setCursor(0,0);
   lcd_key = read_LCD_buttons();  // read the buttons
 
   switch (lcd_key)               // depending on which button was pushed, we perform an action
   {
-    case btnRIGHT:
-    {
-      sl_scrl++;
-      lcd.print("sl_scrl = "); lcd.print(sl_scrl); lcd.print("      ");
-      delay(200);
+  case btnRIGHT:
+  {
+    sl_scrl++;
+    lcd.print("sl_scrl = ");
+    lcd.print(sl_scrl);
+    lcd.print("      ");
+    delay(200);
+    break;
+  }
+  case btnLEFT:
+  {
+    sl_scrl--;
+    lcd.print("sl_scrl = ");
+    lcd.print(sl_scrl);
+    lcd.print("      ");
+    delay(200);
+    break;
+  }
+  case btnUP:
+  {
+    dy_t++;
+    lcd.print("dy_t = ");
+    lcd.print(dy_t);
+    lcd.print("      ");
+    delay(200);
+    break;
+  }
+  case btnDOWN:
+  {
+    dy_t--;
+    lcd.print("dy_t = ");
+    lcd.print(dy_t);
+    lcd.print("      ");
+    delay(200);
+    break;
+  }
+  case btnSELECT:
+  {
+    // switch operation mode
+    cur_mode++;
+    if (cur_mode == MODE_LAST) cur_mode = 0; // wrap around
+    lcd.print("MODE: ");
+    switch (cur_mode) {
+    case MODE_SCROLL:
+      lcd.print("SCROLL   ");
+      dy_t = 5;
       break;
-    }
-    case btnLEFT:
-    {
-      sl_scrl--;
-      lcd.print("sl_scrl = "); lcd.print(sl_scrl); lcd.print("      ");
-      delay(200);
+    case MODE_POINTER:
+      lcd.print("POINTER  ");
+      dy_t = 1;
       break;
-    }
-    case btnUP:
-    {
-      dy_t++;
-      lcd.print("dy_t = "); lcd.print(dy_t); lcd.print("      ");
-      delay(200);
+    default:
       break;
-    }
-    case btnDOWN:
-    {
-      dy_t--;
-      lcd.print("dy_t = "); lcd.print(dy_t); lcd.print("      ");
-      delay(200);
-      break;
-    }
-    case btnSELECT:
-    {
-      // switch operation mode
-      cur_mode++;
-      if (cur_mode == MODE_LAST) cur_mode = 0; // wrap around
-      lcd.print("MODE: ");
-      switch (cur_mode) {
-        case MODE_SCROLL:
-        lcd.print("SCROLL   ");
-        dy_t = 5;
-        break;
-        case MODE_POINTER:
-        lcd.print("POINTER  ");
-        dy_t = 1;
-        break;
-        default: 
-        break;
-            // do something
-          };
-          delay(500);
-          break;
-        }
-      }
+      // do something
+    };
+    delay(500);
+    break;
+  }
+  }
 
-    }
+}
 
 
 
-void setup(void) 
+void setup(void)
 {
-  // set up the LCD's number of columns and rows: 
+  // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
   pinMode(PIN_BACKLIGHT,  OUTPUT);
   digitalWrite(PIN_BACKLIGHT, HIGH);
@@ -209,7 +217,7 @@ void setup(void)
   Mouse.begin();
 }
 
-void loop(void) 
+void loop(void)
 {
   int dy = 0;
   int dx = 0;
@@ -233,9 +241,9 @@ void loop(void)
 
   // we have some minimum pressure we consider 'valid'
   // pressure of 0 means no pressing!
-  if (p.z > ts.pressureThreshhold) 
+  if (p.z > ts.pressureThreshhold)
   {
-    // if (Mouse.isPressed(1) == 0) 
+    // if (Mouse.isPressed(1) == 0)
     // {
     //   Mouse.press(1);
     //   Serial.print("****Press****\n");
@@ -244,43 +252,55 @@ void loop(void)
     // Serial.print("X = "); Serial.print(p.x);
     // Serial.print("\tY = "); Serial.print(p.y);
     // Serial.print("\tPressure = "); Serial.println(p.z);
-    lcd.setCursor(0,1); lcd.print("X");
-    lcd.print(p.x); lcd.print(" Y");
-    lcd.print(p.y); lcd.print(" Z");
-    lcd.print(p.z); lcd.print("  ");
+    lcd.setCursor(0,1);
+    lcd.print("X");
+    lcd.print(p.x);
+    lcd.print(" Y");
+    lcd.print(p.y);
+    lcd.print(" Z");
+    lcd.print(p.z);
+    lcd.print("  ");
 
 
     dx = last_p.x - p.x;
     dy = last_p.y - p.y;
 
-    #ifdef DELTA_PRINT
-    Serial.print("DX = "); Serial.print(dx);
-    Serial.print("\tDY = "); Serial.println(dy); 
-    #endif
+#ifdef DELTA_PRINT
+    Serial.print("DX = ");
+    Serial.print(dx);
+    Serial.print("\tDY = ");
+    Serial.println(dy);
+#endif
 
     if (cur_mode == MODE_SCROLL) // Do thresholding on scroll mode
     {
       CalcMovment_Scroll(dx,dy,&hz_scrl,&vr_scrl);
-      pnt_dx = 0; pnt_dy = 0;
+      pnt_dx = 0;
+      pnt_dy = 0;
     }
     else // POINTER MODE
     {
       CalcMovment_Pointer(dx,dy,&pnt_dx,&pnt_dy);
-      hz_scrl = 0; vr_scrl = 0;
+      hz_scrl = 0;
+      vr_scrl = 0;
     }
 
     if ((vr_scrl != 0) || (hz_scrl != 0) || (pnt_dy != 0) || (pnt_dx != 0)) // passed the threshold send scroll / move event
     {
       Mouse.move(pnt_dx, pnt_dy, -vr_scrl, hz_scrl); // reverse scrolling on android
-      Serial.print("vr_scrl = "); Serial.print(vr_scrl);
-      Serial.print("\thz_scrl = "); Serial.print(hz_scrl);
-      Serial.print("\tpnt_dx = "); Serial.print(pnt_dx);
-      Serial.print("\tpnt_dy = "); Serial.println(pnt_dy);
+      Serial.print("vr_scrl = ");
+      Serial.print(vr_scrl);
+      Serial.print("\thz_scrl = ");
+      Serial.print(hz_scrl);
+      Serial.print("\tpnt_dx = ");
+      Serial.print(pnt_dx);
+      Serial.print("\tpnt_dy = ");
+      Serial.println(pnt_dy);
     }
-    delay(sl_scrl);    
+    delay(sl_scrl);
     last_p = p;
   }
-  else //(p.z > ts.pressureThreshhold) 
+  else //(p.z > ts.pressureThreshhold)
   {
     // if (Mouse.isPressed(1))
     // {
