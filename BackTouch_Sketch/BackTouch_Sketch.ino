0722dcf6dccf06c0b0257b5f0bb07543ab88682a
Gilad Fride <giladfr@gmail.com>
2014-01-19 21:33:41 +0200

    Renamed sketch file

--- BackTouch_Sketch/BackTouch_Sketch.ino
+++ BackTouch_Sketch/BackTouch_Sketch.ino
@@ -10,7 +10,7 @@
 // #define YM 8   // can be a digital pin (Purple)
 // #define XP 9   // can be a digital pin (Red)
 
- #define PLATFORM_MICRO
+#define PLATFORM_MICRO
 // #define PLATFORM_LEONARDO
 
 // PIN defines - Blue 
@@ -52,7 +52,7 @@
 // For better pressure precision, we need to know the resistance
 // between X+ and X- Use any multimeter to read it
 // For the one we're using.
-TouchScreen ts = TouchScreen(XP, YP, XM, YM, 330);
+// TouchScreen ts = TouchScreen(XP, YP, XM, YM, 330);
 Point last_p;
 LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
@@ -73,7 +73,7 @@ typedef enum
   MODE_POINTER,
   MODE_SCROLL,
   MODE_LAST,
-} OperationModes;
+  } OperationModes;
 int cur_mode = MODE_POINTER; // default mode
 
 
@@ -252,67 +252,168 @@ int LCD_action()
 
   switch (lcd_key)               // depending on which button was pushed, we perform an action
   {
-  case btnRIGHT:
-  {
-    sl_scrl++;
-    lcd.print("sl_scrl = ");
-    lcd.print(sl_scrl);
-    lcd.print("      ");
-    delay(200);
-    break;
-  }
-  case btnLEFT:
-  {
-    sl_scrl--;
-    lcd.print("sl_scrl = ");
-    lcd.print(sl_scrl);
-    lcd.print("      ");
-    delay(200);
-    break;
-  }
-  case btnUP:
-  {
-    dy_t++;
-    lcd.print("dy_t = ");
-    lcd.print(dy_t);
-    lcd.print("      ");
-    delay(200);
-    break;
-  }
-  case btnDOWN:
-  {
-    dy_t--;
-    lcd.print("dy_t = ");
-    lcd.print(dy_t);
-    lcd.print("      ");
-    delay(200);
-    break;
-  }
-  case btnSELECT:
-  {
+    case btnRIGHT:
+    {
+      sl_scrl++;
+      lcd.print("sl_scrl = ");
+      lcd.print(sl_scrl);
+      lcd.print("      ");
+      delay(200);
+      break;
+    }
+    case btnLEFT:
+    {
+      sl_scrl--;
+      lcd.print("sl_scrl = ");
+      lcd.print(sl_scrl);
+      lcd.print("      ");
+      delay(200);
+      break;
+    }
+    case btnUP:
+    {
+      dy_t++;
+      lcd.print("dy_t = ");
+      lcd.print(dy_t);
+      lcd.print("      ");
+      delay(200);
+      break;
+    }
+    case btnDOWN:
+    {
+      dy_t--;
+      lcd.print("dy_t = ");
+      lcd.print(dy_t);
+      lcd.print("      ");
+      delay(200);
+      break;
+    }
+    case btnSELECT:
+    {
     // switch operation mode
     cur_mode++;
     if (cur_mode == MODE_LAST) cur_mode = 0; // wrap around
     lcd.print("MODE: ");
     switch (cur_mode) {
-    case MODE_SCROLL:
+      case MODE_SCROLL:
       lcd.print("SCROLL   ");
       dy_t = 5;
       sl_scrl = 5;
       break;
-    case MODE_POINTER:
+      case MODE_POINTER:
       lcd.print("POINTER  ");
       dy_t = 1;
       sl_scrl = 0;
       break;
-    default:
+      default:
       break;
       // do something
     };
     delay(500);
     break;
   }
-  }
+}
+
+}
+
+void I2CWriteByte(char regAddr, char byteToWrite)
+{
+  Wire.beginTransmission(0x48);
+  Wire.write(regAddr);             // sends value byte  
+  Wire.write(byteToWrite);             // sends value byte  
+  Wire.endTransmission();     // stop transmitting
+}
+
+void I2CWrite2Bytes(char regAddr, char firstByte, char secondByte)
+{
+  Wire.beginTransmission(0x48);
+  Wire.write(regAddr);             // sends value byte  
+  Wire.write(firstByte);             // sends value byte  
+  Wire.write(secondByte);             // sends value byte  
+  Wire.endTransmission();     // stop transmitting
+  
+}
+
+
+void FANALInit()
+{
+
+  I2CWriteByte(0x04,0x00);
+  delay(300);
+
+  I2CWriteByte(0x1F,0x00);
+
+  //Touch panel configuration 
+  I2CWriteByte(0x06,0xC);
+  I2CWriteByte(0x07,0x09);
+
+  I2CWrite2Bytes(0x08,0x00,0x8A);
+  I2CWrite2Bytes(0x09,0x00,0x89);
+  I2CWrite2Bytes(0x0A,0x00,0x88);
+  I2CWrite2Bytes(0x0B,0x00,0x8B);
+  I2CWrite2Bytes(0x0C,0x00,0x8C);
+  I2CWrite2Bytes(0x0D,0x00,0x8D);
+  I2CWrite2Bytes(0x0E,0x00,0x8E);
+  I2CWrite2Bytes(0x0F,0x00,0x8F);
+
+  I2CWrite2Bytes(0x10,0x00,0x90);
+  I2CWrite2Bytes(0x11,0x00,0x91);
+  I2CWrite2Bytes(0x12,0x00,0x92);
+  I2CWrite2Bytes(0x13,0x00,0x93);
+  I2CWrite2Bytes(0x14,0x00,0x94);
+
+  //SSD2532 analog setting 
+  I2CWriteByte(0xD5,0x07);
+  I2CWriteByte(0xD8,0x05);
+
+  //Touch detection setting 
+  I2CWriteByte(0x2A,0x07);
+  I2CWriteByte(0x2C,0x01);
+  I2CWriteByte(0x2E,0x0B);
+  I2CWriteByte(0x2F,0x01);
+
+  I2CWriteByte(0x30,0x03);
+  I2CWriteByte(0x31,0x07);
+  I2CWriteByte(0xD7,0x03);
+  I2CWriteByte(0xDB,0x02);
+
+  //Finger recognition criteria 
+  I2CWrite2Bytes(0x33,0x00,0x02);
+  I2CWrite2Bytes(0x34,0x00,0x40);
+  I2CWrite2Bytes(0x35,0x00,0x00);
+  I2CWrite2Bytes(0x36,0xFF,0x36);
+
+  I2CWriteByte(0x37,0x00);
+  I2CWriteByte(0x3D,0x01);
+  I2CWriteByte(0x53,0x16);
+
+  I2CWrite2Bytes(0x54,0x00,0x80);
+
+  I2CWrite2Bytes(0x55,0x00,0x80);
+
+  //Enable moving average filter to smooth fingers output coordinates
+  I2CWriteByte(0x56,0x02);
+  I2CWriteByte(0x58,0x00);
+  I2CWriteByte(0x59,0x01);
+  I2CWriteByte(0x5A,0x01);
+  I2CWriteByte(0x5B,0x03);
+
+  I2CWrite2Bytes(0x66,0x60,0x00);
+  I2CWrite2Bytes(0x67,0x62,0x70);
+
+  I2CWriteByte(0x8A,0x0A);
+
+  I2CWriteByte(0x8B,0x10);
+  I2CWriteByte(0x8C,0xB0);
+
+  I2CWriteByte(0x87,0x01);
+
+
+  // I2CWriteByte(0x23,0x00);
+  // delay(10);
+
+  I2CWriteByte(0x25,0x04);
+  delay(400);
 
 }
 
@@ -320,16 +421,16 @@ int LCD_action()
 
 void setup(void)
 {
-#ifdef PLATFORM_LEONARDO
+  #ifdef PLATFORM_LEONARDO
   // set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  pinMode(PIN_BACKLIGHT,  OUTPUT);
-  digitalWrite(PIN_BACKLIGHT, HIGH);
+  // lcd.begin(16, 2);
+  // pinMode(PIN_BACKLIGHT,  OUTPUT);
+  // digitalWrite(PIN_BACKLIGHT, HIGH);
 
   // Write welcome messege
-  lcd.print("BackTouch V0.2");
+  // lcd.print("BackTouch V0.2");
   Serial.begin(9600);
-#endif
+  #endif
   Wire.begin();        // join i2c bus (address optional for master)
 
   Mouse.begin();
@@ -337,23 +438,27 @@ void setup(void)
 
   pinMode(PIN_TS_INTERRUPT,INPUT);
   pinMode(PIN_TS_RST,OUTPUT);
+  digitalWrite(PIN_TS_RST,LOW);
+  delay(100);
   digitalWrite(PIN_TS_RST,HIGH);
   delay(100);
-  digitalWrite(PIN_TS_RST,LOW);
+
+  FANALInit();
 
 }
 
 
 
 
-volatile char rawTouchPacket[6]; 
+volatile char rawTouchPacket[5]; 
 
 void ReadTouchPacket()
 {
-
-  Wire.requestFrom(0x0A, 6);    // request 6 bytes from slave device #2
+  Wire.beginTransmission(0x48);
+  Wire.write(0x79);
+  Wire.endTransmission();
+  Wire.requestFrom(0x48, 2);    
   int i = 0;
-
   while(Wire.available())    // slave may send less than requested
   { 
     rawTouchPacket[i++] = Wire.read(); // receive a byte as character
@@ -364,14 +469,50 @@ void ReadTouchPacket()
 void ParseTouchPacket(int16_t *x, int16_t *y, int16_t *z,uint8_t *status)
 {
   *status = rawTouchPacket[0] & 1;
-   *x = (rawTouchPacket[1]<<7) | rawTouchPacket[2];
-   *y = (rawTouchPacket[3]<<7) | rawTouchPacket[4];
-   *z = rawTouchPacket[5];
+  *x = (rawTouchPacket[1]<<7) | rawTouchPacket[2];
+  *y = (rawTouchPacket[3]<<7) | rawTouchPacket[4];
+  *z = rawTouchPacket[5];
 }
 
 
 
 void loop(void)
+{
+  I2CWriteByte(0x87,0x01);
+
+  delay(10);
+
+  Wire.beginTransmission(0x48);
+  Wire.write(0x79);
+  Wire.endTransmission();
+  Wire.requestFrom(0x48, 2);
+  Wire.read(); // receive a byte as character
+  Wire.read(); // receive a byte as character
+
+  delay(10);
+
+  Wire.beginTransmission(0x48);
+  Wire.write(0x79);
+  Wire.endTransmission();
+  Wire.requestFrom(0x48, 2);
+  Wire.read(); // receive a byte as character
+  Wire.read(); // receive a byte as character
+
+  delay(10);
+
+  Wire.beginTransmission(0x48);
+  Wire.write(0x79);
+  Wire.endTransmission();
+  Wire.requestFrom(0x48, 2);
+  Wire.read(); // receive a byte as character
+  Wire.read(); // receive a byte as character
+
+
+  delay(1000);
+
+}
+
+void loop1(void)
 {
   int dy = 0;
   int dx = 0;
@@ -391,46 +532,58 @@ void loop(void)
 
   int pinVal;
 
-  while(digitalRead(PIN_TS_INTERRUPT) == 1)
-  {
-    if (isFingerDown == true)
-    {
-      int cur_time = millis();
-      if (cur_time - last_touch_time > RELEASE_THRESHOLD_MILIS)
-      {
-#ifdef SEND_CLICK
-        if (cur_mode == MODE_POINTER) Mouse.release(7);
-#endif
-#ifdef DELTA_PRINT
-        Serial.println("****Release****");
-#endif
-        isFingerDown = false;
-        // Clear all the static arrays and the last point       
-        memset(&x_arr,0,sizeof(int)*AVG_NUM_OF_POINTS);
-        memset(&y_arr,0,sizeof(int)*AVG_NUM_OF_POINTS);
-        memset(&z_arr,0,sizeof(int)*AVG_NUM_OF_POINTS);
-        memset(&last_p,0,sizeof(last_p));
+  // while(digitalRead(PIN_TS_INTERRUPT) == 1)
+  // {
+  //   if (isFingerDown == true)
+  //   {
+  //     int cur_time = millis();
+  //     if (cur_time - last_touch_time > RELEASE_THRESHOLD_MILIS)
+  //     {
+  //       #ifdef SEND_CLICK
+  //       if (cur_mode == MODE_POINTER) Mouse.release(7);
+  //       #endif
+  //       #ifdef DELTA_PRINT
+  //       Serial.println("****Release****");
+  //       #endif
+  //       isFingerDown = false;
+  //       // Clear all the static arrays and the last point       
+  //       memset(&x_arr,0,sizeof(int)*AVG_NUM_OF_POINTS);
+  //       memset(&y_arr,0,sizeof(int)*AVG_NUM_OF_POINTS);
+  //       memset(&z_arr,0,sizeof(int)*AVG_NUM_OF_POINTS);
+  //       memset(&last_p,0,sizeof(last_p));
 
-      }
-    }
+  //     }
+  //   }
 
-  };
+  // };
 
   ReadTouchPacket();
+  // I2CWriteByte(0x86,0);
+
+
+  delay(1000);
+  // Serial.print(rawTouchPacket[0]);
+  // Serial.print(rawTouchPacket[1]);
+  // Serial.print(rawTouchPacket[2]);
+  // Serial.print(rawTouchPacket[3]);
+  // Serial.println(rawTouchPacket[4]);
+
+
+  return;
   ParseTouchPacket(&p.x,&p.y,&p.z,&s);
   // Serial.println(s);
 
 
-#ifdef PLATFORM_LEONARDO
+  #ifdef PLATFORM_LEONARDO
   LCD_action();
-#endif
+  #endif
 
   // we have some minimum pressure we consider 'valid'
   // pressure of 0 means no pressing!
   if ((s == 1))
   {
 
-#ifdef PLATFORM_LEONARDO
+    #ifdef PLATFORM_LEONARDO
     lcd.setCursor(0,1);
     lcd.print("X");
     lcd.print(p.x);
@@ -439,7 +592,7 @@ void loop(void)
     lcd.print(" Z");
     lcd.print(p.z);
     lcd.print("    ");
-#endif
+    #endif
     
     // p = CalcMovingAvg_Simple(p);
 
@@ -447,12 +600,12 @@ void loop(void)
     dy = last_p.y - p.y;
 
 
-#ifdef DELTA_PRINT
+    #ifdef DELTA_PRINT
     Serial.print("DX = ");
     Serial.print(dx);
     Serial.print("\tDY = ");
     Serial.println(dy);
-#endif
+    #endif
 
     if (cur_mode == MODE_SCROLL) // Do thresholding on scroll mode
     {
@@ -472,7 +625,7 @@ void loop(void)
     if ((vr_scrl != 0) || (hz_scrl != 0) || (pnt_dy != 0) || (pnt_dx != 0)) // passed the threshold send scroll / move event
     {
       Mouse.move(pnt_dx, pnt_dy, -vr_scrl, hz_scrl); // reverse scrolling on android
-#ifdef DELTA_PRINT
+      #ifdef DELTA_PRINT
       Serial.print("vr_scrl = ");
       Serial.print(vr_scrl);
       Serial.print("\thz_scrl = ");
@@ -481,12 +634,12 @@ void loop(void)
       Serial.print(pnt_dx);
       Serial.print("\tpnt_dy = ");
       Serial.println(pnt_dy);
-#endif
+      #endif
     }
 
     if (isFingerDown == false)
     {
-#ifdef SEND_CLICK
+      #ifdef SEND_CLICK
       if (cur_mode == MODE_POINTER) 
       {
         Mouse.press(7);
@@ -495,13 +648,13 @@ void loop(void)
         // delay(1);
         // Mouse.press(7);
       }
-#endif
-#ifdef DELTA_PRINT
-       Serial.println("****Press****");
-#endif
-       isFingerDown = true;
+      #endif
+      #ifdef DELTA_PRINT
+      Serial.println("****Press****");
+      #endif
+      isFingerDown = true;
        // last_p = p; // Last point is the same as this point so cursor starts where you left it. 
-    }
+     }
 
     // delay(sl_scrl);
     last_p = p;
